3e430c80a96269c1f1b53ca69b63a28285933c04
Erick Odero <erickarthurs13@gmail.com>
2022-12-25 00:26:16 -0600

    introduced hooks and useContext

---
+++ finance-tracker/src/firebase/config.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDD5WB6IqsCp8O1AJ7gxG3sbpqUjndFwS8",
+    authDomain: "pesa-6c0a2.firebaseapp.com",
+    projectId: "pesa-6c0a2",
+    storageBucket: "pesa-6c0a2.appspot.com",
+    messagingSenderId: "1027976879462",
+    appId: "1:1027976879462:web:0a2a57c5c42d2d0088bca7"
+};
+
+//init firebase
+
+firebase.initializeApp(firebaseConfig)
+
+// init service
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+export {
+    projectFirestore,
+    projectAuth
+}
--- finance-tracker/src/pages/signup/Signup.js
+++ finance-tracker/src/pages/signup/Signup.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import useSignup from '../../hooks/useSignup'
 import styles from './signup.module.css'
 
 
@@ -6,11 +7,12 @@ export default function Signup() {
   const [displayName, setDisplayName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [error, isPending, signup] = useSignup()
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    console.log(displayName, email, password)
+    useSignup(error, isPending, signup)
   }
   return (
     <form onSubmit={handleSubmit} className={styles['signup-form']}>
@@ -39,7 +41,9 @@ export default function Signup() {
           onChange={(e) => setPassword(e.target.value)}
         />
       </label>
-      <button className="btn">Signup</button>
+      {!isPending && <button className="btn">Signup</button>}
+      {isPending && <button className="btn" disabled>Loading...</button>}
+      {error && <p>{error}</p>}
     </form>
   )
 }
